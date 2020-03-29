fc78f8126970a00cef998419ce12c6a9b24d3d50
yowashi <hinzyakushi@gmail.com>
2020-03-29 04:36:08 +0000

    model/controller/route 追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,17 @@
 class LikesController < ApplicationController
 
 	def create
-		@like = Like.new(user_id: @current_user.id, post_id: params(:post_id))
+		@like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
 		@like.save
 		@post = Post.find_by(id: @like.food_id)
-		@like_count = Like.where(food_id: params(:post_id)).count
+		@like_count = Like.where(food_id: params[:post_id]).count
 	end
 
 	def delete
-		@like = Like.find_by(user_id: @current_user.id, post_id: params(:post_id))
+		@like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
 		@post = Post.find_by(id: @like.post_id)
 		@like.destroy
-		@like_count = Like.where(post_id: params(:post_id)).count
+		@like_count = Like.where(post_id: params[:post_id]).count
 	end
 
 end
