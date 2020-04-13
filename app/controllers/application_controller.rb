fe0bbba367bad05d21860d8d8fcbf2023bb8acbf
yowashi <hinzyakushi@gmail.com>
2020-04-13 12:42:13 +0000

    controller追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-	def after_sign_in_path_for(resource)
-  		flash[:notice] ="ログインに成功しました"
-  		posts_path
-  	end
-
-  def after_sign_out_path_for(resource)
-  	root_path
-  end
 end
