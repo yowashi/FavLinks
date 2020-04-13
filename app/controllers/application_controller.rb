ceccc785e1bba8e46178871fffc3eea551beb7f2
yowashi <hinzyakushi@gmail.com>
2020-04-13 12:44:58 +0000

    controller追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+	def after_sign_in_path_for(resource)
+  		posts_path
+  	end
+
+  def after_sign_out_path_for(resource)
+  	root_path
+  end
 end
