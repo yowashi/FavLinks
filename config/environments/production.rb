aac0ad248d87eea8259cfe75bf381722bb4bfce9
yowashi <hinzyakushi@gmail.com>
2020-04-13 09:36:55 +0000

    assetの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,13 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
+  config.assets.digest = true
+  #config.assets.debug = false
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
